3dcb7db1b3d9d752a86f38be100cbf6215ff019c
Kyle Tolle <kyle@nullsix.com>
2015-06-04 00:08:27 -0600

    Only generate the payload once for all webhooks

--- lib/webhooks.rb
+++ lib/webhooks.rb
@@ -4,13 +4,14 @@ require 'json'
 
 module Webhooks
   def send_webhooks(resource, action, data)
+    event_type = "#{resource}:#{action}"
+    payload    = JSON.generate({
+      id:    SecureRandom.uuid,
+      event: event_type,
+      data:  data })
+
     webhooks.each do |webhook|
-      url        = URI.parse(webhook)
-      event_type = "#{resource}:#{action}"
-      payload    = JSON.generate({
-        id:    SecureRandom.uuid,
-        event: event_type,
-        data:  data })
+      url = URI.parse(webhook)
 
       Net::HTTP.start(url.host, url.port) do |http|
         headers       = { 'Content-Type' => 'application/json' }
