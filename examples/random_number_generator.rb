cad8755cb3a07b0d203fc1677e5d851dc8020658
Kyle Tolle <kyle@nullsix.com>
2015-06-03 23:57:49 -0600

    Return the random number generated

--- examples/random_number_generator.rb
+++ examples/random_number_generator.rb
@@ -11,6 +11,8 @@ class RandomNumberGenerator
     random_number = SecureRandom.random_number(1000)
 
     send_webhooks(:random_number, :generate, random_number)
+
+    return random_number
   end
 end
 
