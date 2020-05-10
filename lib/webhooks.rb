d49ff0052ea50bca833b492d63ddc5e702585dc3
Kyle Tolle <kyle@nullsix.com>
2020-05-09 21:55:13 -0600

    Add wip changes

--- lib/webhooks.rb
+++ lib/webhooks.rb
@@ -8,7 +8,6 @@ module Webhooks
   def send_webhooks(resource, action, data)
     event_type = "#{resource}:#{action}"
     payload    = JSON.generate({
-      id:    SecureRandom.uuid,
       event: event_type,
       data:  data })
 
@@ -18,19 +17,6 @@ module Webhooks
       Net::HTTP.start(url.host, url.port) do |http|
         headers       = { 'Content-Type' => 'application/json' }
         response      = http.post('/', payload, headers)
-        response_data = {
-          code:    response.code,
-          body:    response.body
-        }
-
-        puts <<-LOG
-POST request made to #{webhook}
-Payload:
-#{payload}
-Response:
-#{response_data}
-
-        LOG
       end
     end
   end
